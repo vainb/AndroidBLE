719be1d83aa4b44654dc16d2493e6e303850cb79
vainb <j30709@hotmail.com>
2017-02-12 21:52:43 +0800

    Initial commit

---
+++ app/src/main/java/csic/ble/MainActivity.java
@@ -0,0 +1,63 @@
+package csic.ble;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 扫描蓝牙按钮
+    private Button scan_btn;
+    // 蓝牙适配器
+    BluetoothAdapter mBluetoothAdapter;
+    // 蓝牙信号强度
+    private ArrayList<Integer> rssis;
+    // 描述扫描蓝牙的状态
+    private boolean mScanning;
+    private boolean scan_flag;
+    private Handler mHandler;
+    int REQUEST_ENABLE_BT = 1;
+    // 蓝牙扫描时间
+    private static final long SCAN_PERIOD = 10000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initBluetooth();
+    }
+
+    //初始化蓝牙
+    private void initBluetooth() {
+
+        //第一步 检查设备时候支持BLE
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "请注意，您的手机不支持BLE", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        // Initializes Bluetooth adapter.
+        // 获取手机本地的蓝牙适配器
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // 打开蓝牙权限
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBtIntent = new Intent(
+                    BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+}
